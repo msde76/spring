5d89df1d91a8b29f372ae636945c4bc53cc46295
msde76 <95360365+msde76@users.noreply.github.com>
2025-03-01 15:54:46 +0900

    1

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,9 +2,13 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -37,15 +41,29 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
-    
+
     @Scope("singleton")
     static class ClientBean {
+
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider; // ObjectFactory, ObjectProvider
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); // getObject()
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
+    @Scope("singleton")
+    static class ClientBean2 {
         private final PrototypeBean prototypeBean;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
+        public ClientBean2(PrototypeBean prototypeBean) {
             this.prototypeBean = prototypeBean;
         }
         
@@ -54,6 +72,17 @@ public class SingletonWithPrototypeTest1 {
             int count = prototypeBean.getCount(); // ctrl + Alt + N
             return count;
         }
+
+        /*
+        private ApplicationContext ac;
+
+        public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+         */
     }
 
     @Scope("prototype")
